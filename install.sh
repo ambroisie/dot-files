ab3d67f8500fb3df29fa0f0649bd2c8643085499
Bruno BELANYI <bruno@belanyi.fr>
2019-07-11 23:22:16 +0200

    [ADD] Installation script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/sh
+
+# Configuration files
+stow -t ~ X
+stow -t ~ bash
+stow -t ~ emacs
+stow -t ~ git
+stow -t ~ i3
+stow -t ~ i3blocks
+stow -t ~ ranger
+stow -t ~ vim
+stow -t ~ zsh
+
+if ! [ -e ~/scripts ]; then
+    # Only create the symlink when the directory doesn't exist
+    mkdir ~/scripts
+    # Scripts must be in there own directory
+    stow -t ~/scripts scripts/
+fi
+
+# Common shell files
+stow -t ~ shell/
+
+# Wallpapers
+stow -t ~ wallpapers/
